e33d5a5c86591542a17964c4ca6eb0ff4c357539
hanisyamimi02 <158983136+hanisyamimi02@users.noreply.github.com>
2024-02-06 16:04:13 +0800

    Add files via upload

---
+++ sispa/index.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>W3.CSS Template</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+
+body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
+</style>
+</head>
+<body class="w3-light-grey w3-content" style="max-width:1600px">
+
+
+
+
+
+
+
+<!-- Sidebar/menu -->
+<nav class="w3-sidebar w3-collapse w3-white w3-animate-left" style="z-index:3;width:300px;" id="mySidebar"><br>
+  <div class="w3-container">
+    <a href="#" onclick="w3_close()" class="w3-hide-large w3-right w3-jumbo w3-padding w3-hover-grey" title="close menu">
+      <i class="fa fa-remove"></i>
+    </a>
+    <img src="Logo-JPAM.png" style="width:45%;" class="w3-round"><br><br>
+    <h4><b>SISPA</b></h4>
+    <p class="w3-text-grey">SISWA SISWI PERTAHANAN AWAM (SISPA)</p>
+  </div>
+  <div class="w3-bar-block">
+    <a href="#portfolio" onclick="w3_close()" class="w3-bar-item w3-button w3-padding w3-text-teal"><i class="fa fa-th-large fa-fw w3-margin-right"></i>HOME</a> 
+    <a href="#portfolio" onclick="w3_close()" class="w3-bar-item w3-button w3-padding w3-text-teal"><i class="fa fa-th-large fa-fw w3-margin-right"></i>ABOUT SISPA</a> 
+    <a href="#portfolio" onclick="w3_close()" class="w3-bar-item w3-button w3-padding w3-text-teal"><i class="fa fa-th-large fa-fw w3-margin-right"></i>MEMBERSHIP</a> 
+    <a href="#portfolio" onclick="w3_close()" class="w3-bar-item w3-button w3-padding w3-text-teal"><i class="fa fa-th-large fa-fw w3-margin-right"></i>ACTIVITIES</a> 
+    <a href="#portfolio" onclick="w3_close()" class="w3-bar-item w3-button w3-padding w3-text-teal"><i class="fa fa-th-large fa-fw w3-margin-right"></i>ACHIEVEMENT</a>
+    <a href="#about" onclick="w3_close()" class="w3-bar-item w3-button w3-padding"><i class="fa fa-user fa-fw w3-margin-right"></i>ABOUT</a> 
+    <a href="#contact" onclick="w3_close()" class="w3-bar-item w3-button w3-padding"><i class="fa fa-envelope fa-fw w3-margin-right"></i>CONTACT</a>
+    <a href="admin/index.php" onclick="w3_close()" class="w3-bar-item w3-button w3-padding w3-text-teal"><i class="fa fa-th-large fa-fw w3-margin-right"></i>ADMINISTRATOR</a> 
+
+  </div>
+  <div class="w3-panel w3-large">
+    <i class="fa fa-facebook-official w3-hover-opacity"></i>
+    <i class="fa fa-instagram w3-hover-opacity"></i>
+    <i class="fa fa-snapchat w3-hover-opacity"></i>
+    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+    <i class="fa fa-twitter w3-hover-opacity"></i>
+    <i class="fa fa-linkedin w3-hover-opacity"></i>
+  </div>
+</nav>
+
+<!-- Overlay effect when opening sidebar on small screens -->
+<div class="w3-overlay w3-hide-large w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+<!-- !PAGE CONTENT! -->
+<div class="w3-main" style="margin-left:300px">
+
+  <!-- Header -->
+  <header id="portfolio">
+    <a href="#"><img src="/w3images/avatar_g2.jpg" style="width:65px;" class="w3-circle w3-right w3-margin w3-hide-large w3-hover-opacity"></a>
+    <span class="w3-button w3-hide-large w3-xxlarge w3-hover-text-grey" onclick="w3_open()"><i class="fa fa-bars"></i></span>
+    <div class="w3-container">
+    <h1><b>SISWA SISWI PERTAHANAN AWAM (SISPA)</b></h1>
+    <hr>
+
+  
+
+
+    <h4>ABOUT SISPA</h4>
+    <p><b>Sispa is known as a public defense student who was also established at the university and its involvement by female students at the university itself. Sispa includes various aspects of learning that can be learned, especially external skills such as emergency aid, first aid, and many more. So join our club to gain more knowledge, skills and many advantages.</b></p>
+    <hr>
+    <hr>
+  
+
+    <h4>SISPA MEMBERSHIP</b></h4>
+    <!-- Progress bars / Skills -->
+    <p><b>Here are the highest councilors and a handful of Exco leaders for the 2022/2023 session. Those who lead the organization in the following year and also manage the entry of new members who want to be active together or want to venture into the field and experience in this SISPA (SISWA SISWI PERTAHANAN AWAM).</b></p>
+<hr>
+    <center><img src="yas1.jpg" alt="Me" style="width:15%">
+<img src="iqbal1.jpg" alt="Me" style="width:15%"><img src="intan1.jpg" alt="Me" style="width:15%"><img src="irfan1.jpg" alt="Me" style="width:15%"> </center>
+
+
+
+
+<hr>
+<hr>
+<hr>
+
+
+    <h4>ACTIVITIES</b></h4>
+    <!-- Progress bars / Skills -->
+    <hr>
+<center><img src="sispa1.jpg" alt="Me" style="width:40%"><center>
+<p><b>"The prime morning line together with other corps units that participated with the rector and deputy rector"</b></p>
+<r>
+
+  <hr>
+  <img src="sispa2.jpg" alt="Me" style="width:40%"><center>
+<p><b>"The disaster management program collaborates with the Machang District Civil Defense Force (APM)"</b></p>
+
+<hr>
+    <img src="sispa3.jpg" alt="Me" style="width:40%">
+<center>
+<p><b>"Take courses to meet the requirements"</b></p>
+
+
+<hr>
+<hr>
+<hr>
+
+
+      <h4>AVHIEVEMENT</h4>
+    <!-- Progress bars / Skills -->
+    <hr>
+<center><img src="ATS1.jpg" alt="Me" style="width:15%">
+<img src="ATS2.jpg" alt="Me" style="width:15%"></center>
+<hr>
+<hr>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    <h4>SELECTION JOINING</h4>
+    <!-- Pricing Tables -->
+    <hr>
+    <div class="w3-row-padding" style="margin:0 -16px">
+      <div class="w3-third w3-margin-bottom">
+        <ul class="w3-ul w3-border w3-white w3-center w3-opacity w3-hover-opacity-off">
+          <li class="w3-black w3-xlarge w3-padding-32">Ordinary Members</li>
+          <li class="w3-padding-16">Curriculum Marks Attend</li>
+          <li class="w3-padding-16">Class ever week</li>
+          <li class="w3-padding-16">3 semester only</li>
+          <li class="w3-light-grey w3-padding-24">
+            <button class="w3-button w3-teal w3-padding-large w3-hover-black">Sign Up</button>
+          </li>
+        </ul>
+      </div>
+      <div class="w3-third w3-margin-bottom">
+        <ul class="w3-ul w3-border w3-white w3-center w3-opacity w3-hover-opacity-off">
+          <li class="w3-teal w3-xlarge w3-padding-32">Commander Members</li>
+          <li class="w3-padding-16">Commissioned Registered</li>
+          <li class="w3-padding-16">More benefits</li>
+          <li class="w3-padding-16">5 semester activation</li>
+          <li class="w3-light-grey w3-padding-24">
+            <button class="w3-button w3-teal w3-padding-large w3-hover-black">Sign Up</button>
+          </li>
+        </ul>
+      </div>
+      <div class="w3-third w3-margin-bottom">
+        <ul class="w3-ul w3-border w3-white w3-center w3-opacity w3-hover-opacity-off">
+          <li class="w3-black w3-xlarge w3-padding-32">Active Members</li>
+          <li class="w3-padding-16">Curriculum Marks Attend</li>
+          <li class="w3-padding-16">Class ever Tuesday</li>
+          <li class="w3-padding-16">3 semester only</li>
+          <li class="w3-light-grey w3-padding-24">
+            <button class="w3-button w3-teal w3-padding-large w3-hover-black">Sign Up</button>
+          </li>
+        </ul>
+      </div>
+
+
+
+
+  <!-- Contact Section -->
+  <div class="w3-container w3-padding-large w3-grey">
+    <h4 id="contact"><b>Contact Us</b></h4>
+    <div class="w3-row-padding w3-center w3-padding-24" style="margin:0 -16px">
+      <div class="w3-third w3-dark-grey">
+        <p><i class="fa fa-envelope w3-xxlarge w3-text-light-grey"></i></p>
+        <p>sispauitmck@email.com</p>
+      </div>
+      <div class="w3-third w3-teal">
+        <p><i class="fa fa-map-marker w3-xxlarge w3-text-light-grey"></i></p>
+        <p>Machang, Kelantan</p>
+      </div>
+      <div class="w3-third w3-dark-grey">
+        <p><i class="fa fa-phone w3-xxlarge w3-text-light-grey"></i></p>
+        <p>Fax: 09566610</p>
+      </div>
+    </div>
+    <hr class="w3-opacity">
+    <form action="/action_page.php" target="_blank">
+      <div class="w3-section">
+        <label>Name</label>
+        <input class="w3-input w3-border" type="text" name="Name" required>
+      </div>
+      <div class="w3-section">
+        <label>Email</label>
+        <input class="w3-input w3-border" type="text" name="Email" required>
+      </div>
+      <div class="w3-section">
+        <label>Message</label>
+        <input class="w3-input w3-border" type="text" name="Message" required>
+      </div>
+      <button type="submit" class="w3-button w3-black w3-margin-bottom"><i class="fa fa-paper-plane w3-margin-right"></i>Send Message</button>
+    </form>
+  </div>
+
+
+
+
+
+
+
+
+
+  <!-- Footer -->
+  <footer class="w3-container w3-padding-32 w3-dark-grey">
+  <div class="w3-row-padding">
+    <div class="w3-third">
+      <h3>FOOTER</h3>
+      <p>Come to join us in this club</p>
+      <b>“SEDIA PANTAS BERINTEGRITI”</b>
+      <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+    </div>
+  
+    <div class="w3-third">
+      <h3>BLOG POSTS</h3>
+      <ul class="w3-ul w3-hoverable">
+        <li class="w3-padding-16">
+          <img src="sispa1.jpg" class="w3-left w3-margin-right" style="width:50px">
+          <span class="w3-large">Facebook: SispaUiTMCK</span><br>
+          <span></span>
+        </li>
+        <li class="w3-padding-16">
+          <img src="sispa2.jpg" class="w3-left w3-margin-right" style="width:50px">
+          <span class="w3-large">Instagram: SispaUiTMCK</span><br>
+          <span></span>
+        </li> 
+      </ul>
+    </div>
+
+   
+
+  </div>
+  </footer>
+  
+  <div class="w3-black w3-center w3-padding-24">Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-opacity">w3.css</a></div>
+
+<!-- End page content -->
+</div>
+
+<script>
+// Script to open and close sidebar
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+ 
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
+</script>
+
+</body>
+</html>
